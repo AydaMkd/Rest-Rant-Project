dc139e3e70ab70add5d252b68c6a1ee8a48fd66c
MyMac <mymac@MyMacs-MBP.lan>
2022-02-23 16:43:56 -0600

    commit

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require("express");
+const app = express();
+app.use("/places", require("./controllers/places"));
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
-})
-
-app.listen(3000);
-
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-
-app.listen(process.env.PORT)
-
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+});
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).send("<h1>404 Page</h1>")
 })
 
 app.listen(process.env.PORT)
